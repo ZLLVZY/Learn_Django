925777f94dfe6ef91a880928d02b5dafa8694aa1
ZLLVZY <p752101119@163.com>
2020-03-19 21:16:39 +0800

    add TestCase

--- student_house/student_sys/student/tests.py
+++ student_house/student_sys/student/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from django.test import Client
 
+from .models import Student
 # Create your tests here.
+class StudentTestCase(TestCase):
+    def setUp(self):
+        Student.objects.create(
+                name='ZL',
+                sex=1,
+                email='zl@123456.com',
+                profession='Protoss',
+                qq='123456',
+                phone='13579',
+                )
+    
+    def test_create_and_sex_show(self):
+        Student.objects.create(
+                name='ZY',
+                sex=2,
+                email='zy@123456.com',
+                profession='cute',
+                qq='123456',
+                phone='13579',
+                )
+        self.assertEqual(student.sex_show,'女','性别字段内容跟展示不一致！')
+
+    def test_filter(self):
+        Student.objects.create(
+                name='ZY',
+                sex=2,
+                email='zy@123456.com',
+                profession='cute',
+                qq='123456',
+                phone='13579',
+                )
+        name='ZY'
+        students=Students.objects.filter(name=name)
+        self.assertEqual(students.count(),1,'应该只存在一个名称为{}的记录'.format(name))
+
+    def test_get_index(self):
+        clinet=Client()
+        response=client.get('/')
+        self.assertEqual(response.status_code,200,'status code must be 200!')
+
+    def test_post_student(self):
+        """docstring for test_post_student"""
+        client=Client()
+        data=dict(
+                name='test_for_post',
+                sex=1,
+                email='test@post.com',
+                profession='Test',
+                qq='123456',
+                phone='1345',
+                )
+        response=client.post('/',data)
+        self.assertEqual(response.status_code,302,'status code must be 302!')
+
+        response=client.get('/')
+        self.assertTrue(b'test_for_post' in response.content,
+                        'response content must contain `test_for_post`')
